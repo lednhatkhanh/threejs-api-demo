b2806230e9b0eab3f3ddf3bb62faa8f179281500
Khanh Le <lednhatkhanh@gmail.com>
2019-07-17 09:03:17 +0700

    Update config

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
-console.log(path.resolve(process.cwd(), "public"));
-
 app.use(cors());
 app.use(compression({ filter: () => true }));
 app.use(express.static(path.resolve(process.cwd(), "public")));
