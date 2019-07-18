4d53318cc7a7b409f80af7d947609637c6fe1d54
Khanh Le <lednhatkhanh@gmail.com>
2019-07-18 10:37:29 +0700

    Add brotli compression

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const compression = require("compression");
+// const compression = require("compression");
 const cors = require("cors");
 const path = require("path");
+const shrinkRay = require("shrink-ray");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(compression({ filter: () => true }));
+// app.use(compression({ filter: () => true }));
+app.use(
+  shrinkRay({
+    filter() {
+      return true;
+    },
+  }),
+);
+
 app.use(express.static(path.resolve(process.cwd(), "public")));
+
 app.get("/", (_req, res) => res.json(JSON.stringify(JSON.parse({ hello: "World" }))));
 
 app.listen(port, () => {
